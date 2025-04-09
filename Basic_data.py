02d687a78eb2af59a34d7adb289d7dd8444fcb89
Naton-Cai <natoncai@gmail.com>
2025-04-08 17:22:31 -0700

    added the Basic URL reader for Part B

---
+++ Basic_data.py
@@ -0,0 +1,18 @@
+import urllib.request as request
+import urllib.error as error
+import datetime
+
+#our base Url
+baseUrl = "https://busdata.cs.pdx.edu/api/getBreadCrumbs?vehicle_id="
+#reads vehicleids.txt and parses into a list for each id entry
+vehicleIds = [line.strip() for line in open("vehicleids.txt","r").readlines()]
+
+
+for id in vehicleIds:
+    url = baseUrl + id
+    try:
+        #retrieves the data for the assocaited ID and then download it into the Data Folder
+        request.urlretrieve(url, f"Data/{datetime.date.today()}_{id}.json")
+    except error.HTTPError as e:
+            print(f"{e} FOR {id}")
+    
