e031adb2d46f0d1c91bbf26ebff9efbb97fbf560
John Pham <johpham@pdx.edu>
2025-04-09 15:20:21 -0700

    Added Data Logging

--- Basic_data.py
+++ Basic_data.py
@@ -2,11 +2,26 @@ import urllib.request as request
 import urllib.error as error
 import datetime
 import os
+import dataLogging as log
+
 
 #our base Url
 baseUrl = "https://busdata.cs.pdx.edu/api/getBreadCrumbs?vehicle_id="
 #reads vehicleids.txt and parses into a list for each id entry
 vehicleIds = [line.strip() for line in open("vehicleids.txt","r").readlines()]
+
+
+#Error Counter
+errorCount = 0
+
+#Current Date, Weekday, Time of Collection Start
+currDate = datetime.date.today()
+day = currDate.strftime("%A")
+time = datetime.datetime.now().strftime("%H:%M:%S")
+sensorReadings = "n/a"
+totalData = "n/a"
+pubsubCount = "n/a"
+
 
 path = "./Data/" + str(datetime.date.today())
 if not os.path.exists(path):
@@ -19,8 +34,17 @@ if not os.path.exists(path):
             request.urlretrieve(url, f"Data/{datetime.date.today()}/{datetime.date.today()}_{id}.json")
         except error.HTTPError as e:
             print(f"{e} FOR {id}")
+            errorCount += 1 #Adds to Error Count
+    
+    #Logs Data After Collection
+    data = [{"date": currDate, "day_of_week": day, "time_accessed": time, "#_sensor_readings": sensorReadings, "total_data_saved_(KBs)": totalData, "#_pub/sub_message_published/recieved": pubsubCount}]
+    log.dataLog(data)
+
+
 else:
     print("Data Aready Collected for: " + str(datetime.date.today()))
 
+field_headers = ["date","day_of_week", "time_accessed", "#_sensor_readings", "total_data_saved_(KBs)", "#_pub/sub_message_published/recieved"]
+data = [{"date": currDate, "day_of_week": day, "time_accessed": time, "#_sensor_readings": sensorReadings, "total_data_saved_(KBs)": totalData, "#_pub/sub_message_published/recieved": pubsubCount}]
 
     
---
+++ dataLogging.py
@@ -0,0 +1,15 @@
+import csv
+import os
+
+def dataLog(data):
+
+    if not os.path.exists("dataLog.csv"):
+        field_headers = ["date","day_of_week", "time_accessed", "#_sensor_readings", "total_data_saved_(KBs)", "#_pub/sub_message_published/recieved"]
+        with open("dataLog.csv", 'w', newline='') as file:
+            writer = csv.DictWriter(file, fieldnames=field_headers)
+            writer.writeheader()
+            writer.writerows(data)
+    else:
+        with open("dataLog.csv", "a") as file:
+            writer = csv.DictWriter(file, fieldnames=field_headers)
+            writer.writerows(data)
