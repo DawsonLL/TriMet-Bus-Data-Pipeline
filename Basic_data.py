3ac85573ed82fdf82c368347c1d9af3daa53c564
John Pham <johpham@pdx.edu>
2025-04-09 11:52:21 -0700

    Changed File Format

--- Basic_data.py
+++ Basic_data.py
@@ -1,6 +1,7 @@
 import urllib.request as request
 import urllib.error as error
 import datetime
+import os
 
 #our base Url
 baseUrl = "https://busdata.cs.pdx.edu/api/getBreadCrumbs?vehicle_id="
@@ -12,7 +13,8 @@ for id in vehicleIds:
     url = baseUrl + id
     try:
         #retrieves the data for the assocaited ID and then download it into the Data Folder
-        request.urlretrieve(url, f"Data/{datetime.date.today()}_{id}.json")
+        request.urlretrieve(url, f"Data/{datetime.date.today()}/{datetime.date.today()}_{id}.json")
     except error.HTTPError as e:
             print(f"{e} FOR {id}")
+
     
