6c513bc1621b76c0978d5782c9652a1776968cf6
John Pham <johpham@pdx.edu>
2025-04-28 14:20:58 -0700

    Combined Gather and Publish.

--- Publish.py
+++ Publish.py
@@ -9,15 +9,15 @@ def future_callback(future):
         # Wait for the result of the publish operation.
         future.result()  
     except Exception as e:
-        print(f"An error occurred: {e}")
+        print(f"An error occurred in future_callback: {e}")
 
 #takes the date and opens the folder corosponding to that date and reads all of the jsons in that folder
 #then publishes them to the Trimet_IHS topic
-def Publish_PubSub(date):
+def Publish_PubSub(data):
     start_time = datetime.datetime.now()
     #topic
-    project_id = "data-eng-456119"
-    topic_id = "Trimet_IHS"
+    project_id = "DataEng-S25"
+    topic_id = "my-topic"
     publish_count = 0
     future_list = []
 
@@ -25,20 +25,16 @@ def Publish_PubSub(date):
     # The `topic_path` method creates a fully qualified identifier
     # in the form `projects/{project_id}/topics/{topic_id}`
     topic_path = publisher.topic_path(project_id, topic_id)
+    for item in data:
+        # Data must be a bytestring
+        datastr = json.dumps(item).encode("utf-8")
 
-    for filename in os.listdir(f"{os.getcwd()}/Data/{date}"):
-        with open(os.path.join(f"{os.getcwd()}/Data/{date}", filename), 'r') as file:
-                data = json.load(file)
-                for item in data:
-                    # Data must be a bytestring
-                    datastr = json.dumps(item).encode("utf-8")
+        future = publisher.publish(topic_path, datastr)
+        future.add_done_callback(future_callback)
+        #add it to the list of futures
+        future_list.append(future)
 
-                    future = publisher.publish(topic_path, datastr)
-                    future.add_done_callback(future_callback)
-                    #add it to the list of futures
-                    future_list.append(future)
-
-                    publish_count+= 1
+        publish_count+= 1
 
     end_time = datetime.datetime.now()
     run_time = end_time - start_time
@@ -47,5 +43,5 @@ def Publish_PubSub(date):
     for future in futures.as_completed(future_list):
         continue
 
-    print(f"Published {publish_count} messages to {topic_path} in {run_time}.")
+    #print(f"Published {publish_count} messages to {topic_path} in {run_time}.")
     return int(publish_count)
