09c5db1fb82e0b73c27ea38ab2aa8117d96f0d91
Naton-Cai <natoncai@gmail.com>
2025-05-02 18:18:18 -0700

    workded on validation

--- Validate-Transform.py
+++ Validate-Transform.py
@@ -48,6 +48,7 @@ error_count = 0
 log_file = "./Received_Data/2025-04-11.json"
 with open(log_file, "r", encoding="utf-8") as f:
 
+    #we need to pull out the nested data from the json
     df = pd.read_json(log_file)
     expanded = df['data'].apply(eval_to_df)
     messages = pd.concat(expanded.tolist(), ignore_index=True)
@@ -64,6 +65,28 @@ with open(log_file, "r", encoding="utf-8") as f:
     #split the dataframe into multiples based off ID
     vehicle_groups = {vehicle_id: group for vehicle_id, group in messages.groupby('VEHICLE_ID')}
     for vehicle_id in vehicle_groups:
-        vehicle_groups[vehicle_id].sort_values(by='ACT_TIME', inplace=True)
         for index, row in vehicle_groups[vehicle_id].iterrows():
-            print(row["VEHICLE_ID"], row["ACT_TIME"], row["EVENT_NO_TRIP"] )
+            Indvidual_Validation(row)
+
+        vehicle_groups[vehicle_id].sort_values(by='ACT_TIME', inplace=True)
+
+        #drops the unused columns
+        vehicle_groups[vehicle_id] = vehicle_groups[vehicle_id].drop("GPS_SATELLITES", axis='columns')
+        vehicle_groups[vehicle_id] = vehicle_groups[vehicle_id].drop("GPS_HDOP", axis='columns')
+
+        #create the timestamp
+        vehicle_groups[vehicle_id]['TIMESTAMP'] = vehicle_groups[vehicle_id].apply(lambda row: pd.to_datetime(row['OPD_DATE'], format="%d%b%Y:%H:%M:%S") + pd.to_timedelta(row['ACT_TIME'], unit='s'), axis=1)
+        vehicle_groups[vehicle_id] = vehicle_groups[vehicle_id].drop('OPD_DATE', axis='columns')
+        vehicle_groups[vehicle_id] = vehicle_groups[vehicle_id].drop('ACT_TIME', axis='columns')
+
+        #calculates the time
+        vehicle_groups[vehicle_id]['dMETERS'] = vehicle_groups[vehicle_id].groupby("VEHICLE_ID")['METERS'].diff()
+        vehicle_groups[vehicle_id]['dTIMESTAMP'] =  vehicle_groups[vehicle_id].groupby("VEHICLE_ID")['TIMESTAMP'].diff().dt.total_seconds()
+        vehicle_groups[vehicle_id]['SPEED'] = vehicle_groups[vehicle_id].apply(lambda row: row['dMETERS'] / row['dTIMESTAMP'], axis=1)
+        vehicle_groups[vehicle_id] = vehicle_groups[vehicle_id].drop(columns=['dMETERS', 'dTIMESTAMP'])
+
+        #sets the the first row speed equal to the second
+        if len(vehicle_groups[vehicle_id]) > 1:
+            vehicle_groups[vehicle_id].iloc[0, vehicle_groups[vehicle_id].columns.get_loc('SPEED')] = vehicle_groups[vehicle_id].iloc[1]['SPEED']
+
+        print(vehicle_groups[vehicle_id])
