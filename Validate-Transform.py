d62cf709380d31cffe14c48e3f93e71a0b304838
johpham@cecs.pdx.edu <johpham@pdx.edu>
2025-05-08 21:28:07 -0700

    Now returns a single df instead of a dic of dfs

--- Validate-Transform.py
+++ Validate-Transform.py
@@ -1,12 +1,14 @@
 import pandas as pd
 import ast
+import logging
 
 #conversts the string literal to a proper panda dataframe
 def eval_to_df(x):
     try:
+        x = x.replace("null", "0")
         return pd.json_normalize(ast.literal_eval(x))
     except Exception as e:
-        pass
+        logging.error(f"An error occurred: {e} {x}")
 
 
 #Validation for indvidual validation, if not satisfy  return False
@@ -14,42 +16,37 @@ def Indvidual_Validation(message_data):
 
     column_array = ["EVENT_NO_TRIP", "EVENT_NO_STOP", "OPD_DATE", "VEHICLE_ID", "METERS", "ACT_TIME", "GPS_LONGITUDE", "GPS_LATITUDE", "GPS_SATELLITES", "GPS_HDOP"]
 
-    try:
-        #techincally literal_eval encounters an error with the null values already which should be caught by the error but adding each check should be more thorough 
-        message_dic = ast.literal_eval(message_data)
-    except Exception as e:
-        return False
-
     #existence validation for each column
     for column_name in column_array:
-        if message_dic.get(column_name, False) == None:
-            return False
+        message_data[column_name] = message_data.get(column_name, 0)
     
-    #GPS_LATITUDE should be between 42°N (42) to 46° 15'N (46.25)  (the rough borders of oregon)
-    if message_dic["GPS_LATITUDE"] > 46.25 or message_dic["GPS_LATITUDE"] < 42:
-        return False
-    #GPS_LATITUDE should be between 116° 45'W (-116.75) to 124° 30'W (-124.5) (the rough borders of oregon)
-    if  message_dic["GPS_LONGITUDE"] > -116.75 or message_dic["GPS_LONGITUDE"] < -124.5:
-        return False
-    
-    #GPS_SATELLITES is always greater than 4
-    if message_dic["GPS_SATELLITES"] < 4.0:
-        return False
+    #GPS_LATITUDE should be between 42째N (42) to 46째 15'N (46.25)  (the rough borders of oregon)
+    message_data["GPS_LATITUDE"] = min(42, max(message_data["GPS_LATITUDE"], 46))
+
+    #GPS_LONGITUDE should be between 116째 45'W (-116.75) to 124째 30'W (-124.5) (the rough borders of oregon)
+    message_data["GPS_LONGITUDE"] = min(-124.5, max(message_data["GPS_LATITUDE"], -116.75))
     
     #ACT_TIME does not exceed 24 hours
-    if message_dic["ACT_TIME"] > 2073600:
-        return False
-        
-
+    message_data["ACT_TIME"] = min(message_data["ACT_TIME"], 2073600)
+    
+    return message_data
+    
 def Transform(messages):
     #for index, row in messages.iterrows():
     error_count = 0
 
-    #split the dataframe into multiples based off ID
+    #There are no messages that are exact duplicates of eachother    
+    messages.drop_duplicates()
+
+    #split the dataframe into multiples based off TRIP ID to make calucate them easier, then recombine them later
     trip_groups = {trip_id: group for trip_id, group in messages.groupby('EVENT_NO_TRIP')}
     for trip_id in trip_groups:
+
+        #Each EVENT_NO_TRIP is assigned to only one VEHICLE_ID 
+        trip_groups[trip_id]["VEHICLE_ID"] = trip_groups[trip_id]["VEHICLE_ID"].mode()[0]
+
         for index, row in trip_groups[trip_id].iterrows():
-            Indvidual_Validation(row)
+            trip_groups[trip_id].loc[index] = Indvidual_Validation(row)
 
         #drops the unused columns
         trip_groups[trip_id] = trip_groups[trip_id].drop("GPS_SATELLITES", axis='columns')
@@ -62,34 +59,41 @@ def Transform(messages):
 
         trip_groups[trip_id].sort_values(by='TIMESTAMP', inplace=True)
 
-        #calculates the time
+        #calculates the speed
         trip_groups[trip_id]['dMETERS'] = trip_groups[trip_id].groupby("EVENT_NO_TRIP")['METERS'].diff()
         trip_groups[trip_id]['dTIMESTAMP'] =  trip_groups[trip_id].groupby("EVENT_NO_TRIP")['TIMESTAMP'].diff().dt.total_seconds()
         trip_groups[trip_id]['SPEED'] = trip_groups[trip_id].apply(lambda row: row['dMETERS'] / row['dTIMESTAMP'], axis=1)
-        trip_groups[trip_id] = trip_groups[trip_id].drop(columns=['dMETERS', 'dTIMESTAMP'])
+        trip_groups[trip_id] = trip_groups[trip_id].drop(columns=['dMETERS', 'dTIMESTAMP', 'METERS'])
 
         #sets the the first row speed equal to the second
         if len(trip_groups[trip_id]) > 1:
             trip_groups[trip_id].iloc[0, trip_groups[trip_id].columns.get_loc('SPEED')] = trip_groups[trip_id].iloc[1]['SPEED']
         
-        trip_groups[trip_id]['route_id'] = None
-        trip_groups[trip_id]['service_key'] = None
-        trip_groups[trip_id]['direction'] = None
+        #create the columns we dont have values for
+        trip_groups[trip_id]['route_id'] = 0
+        trip_groups[trip_id]['service_key'] = 0
+        trip_groups[trip_id]['direction'] = 0
 
-    return trip_groups
+    #recombing them into a singular dataframe
+    messages = pd.concat(trip_groups.values(), ignore_index=True)
 
-log_file = "./Received_Data/2025-04-16.json"
+    for index, row in messages.iterrows():
+        #Speed does not exceed 120 MPH (53.6448 Meters per Second)
+        row['SPEED'] = min(row['SPEED'],53.6448)
+
+    return messages
+
+
+
+log_file = "./Received_Data/2025-04-11.json"
 with open(log_file, "r", encoding="utf-8") as f:
-
     #we need to pull out the nested data string from the json
     df = pd.read_json(log_file)
     expanded = df['data'].apply(eval_to_df)
     messages = pd.concat(expanded.tolist(), ignore_index=True)
 
-    #messages.to_csv("dataLog.csv", index=False)
 
-trip_groups = Transform(messages)
 
-if trip_groups:
-    for trip_id in trip_groups:
-        print(trip_groups[trip_id])
+messages = Transform(messages)
+
+print(messages)
